c7c2f164c1ff19dc6825c04efd6d178d471bc63a
vartika1403 <vartika.1403@gmail.com>
2017-06-04 23:29:09 +0530

    registered and unregister the receiver

--- app/src/main/java/com/example/vartikasharma/pushnotificationfcm/MainActivity.java
+++ app/src/main/java/com/example/vartikasharma/pushnotificationfcm/MainActivity.java
@@ -1,13 +1,87 @@
 package com.example.vartikasharma.pushnotificationfcm;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.messaging.FirebaseMessaging;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    private BroadcastReceiver broadcastReceiver;
+    private TextView textMessage, textRegId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textMessage = (TextView) findViewById(R.id.text_notification_message);
+        textRegId = (TextView) findViewById(R.id.text_reg_id);
+        broadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                // checking for type intent filter
+                if (intent.getAction().equals(Config.REGISTRATION_COMPLETE)) {
+                    // gcm successfully registered
+                    // now subscribe to `global` topic to receive app wide notifications
+                    FirebaseMessaging.getInstance().subscribeToTopic(Config.TOPIC_GLOBAL);
+
+                    displayFirebaseRegId();
+
+                } else if (intent.getAction().equals(Config.PUSH_NOTIFICATION)) {
+                    // new push notification is received
+
+                    String message = intent.getStringExtra("message");
+                    Toast.makeText(getApplicationContext(), "Push notification: " + message, Toast.LENGTH_LONG).show();
+                    textMessage.setText(message);
+                }
+            }
+        };
+
+        displayFirebaseRegId();
+    }
+
+    private void displayFirebaseRegId() {
+        SharedPreferences pref = getApplicationContext().getSharedPreferences(Config.SHARED_PREF, 0);
+        String regId = pref.getString("regId", null);
+
+        Log.i(LOG_TAG, "Firebase reg id: " + regId);
+
+        if (!TextUtils.isEmpty(regId))
+            textRegId.setText("Firebase Reg Id: " + regId);
+        else
+            textRegId.setText("Firebase Reg Id is not received yet!");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        // register GCM registartion complete receiver
+        LocalBroadcastManager.getInstance(this).registerReceiver(broadcastReceiver,
+                new IntentFilter(Config.REGISTRATION_COMPLETE));
+
+        // register new push message receiver
+        // by doing this activity get notified each time when new message arrives
+        LocalBroadcastManager.getInstance(this).registerReceiver(broadcastReceiver,
+                new IntentFilter(Config.PUSH_NOTIFICATION));
+
+        // clear the notification when app is opened
+        NotificationUtils.clearNotifications(this);
+    }
+
+    @Override
+    public void onPause() {
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(broadcastReceiver);
+        super.onPause();
     }
 }
